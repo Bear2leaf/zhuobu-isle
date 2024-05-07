f56a0b2c0dfed160333b4109a981536673b58ac4
parks-hordes <parks-hordes.0o@icloud.com>
2024-05-07 17:02:11 +0800

    add Camera class

---
+++ src/camera/Camera.ts
@@ -0,0 +1,57 @@
+import { mat4, vec2, vec3, vec4 } from "gl-matrix";
+import Drawobject from "../drawobject/Drawobject.js";
+
+export default class Camera {
+    private readonly velocity: vec2;
+    private readonly windowInfo: [number, number];
+    private readonly projection: mat4;
+    private readonly view: mat4;
+    private readonly model: mat4;
+    private readonly cameraPosition: vec2;
+    private readonly speed;
+    constructor() {
+        this.cameraPosition = vec2.create();
+        this.velocity = vec2.create();
+        this.windowInfo = [0, 0];
+        this.projection = mat4.create();
+        this.view = mat4.create();
+        this.model = mat4.create();
+        this.speed = 0.1;
+    }
+    setVelocity(x: number, y: number) {
+        this.velocity[0] = x;
+        this.velocity[1] = -y;
+
+    }
+    screenToWorld(x: number, y: number, p: vec4) {
+        const windowInfo = this.windowInfo;
+        vec4.copy(p, vec4.fromValues(x / windowInfo[0], y / windowInfo[1], 0, 1));
+        vec4.sub(p, vec4.mul(p, p, vec4.fromValues(2, 2, 0, 1)), vec4.fromValues(1, 1, 0, 0));
+        p[1] = -p[1];
+        vec4.transformMat4(p, p, mat4.invert(mat4.create(), this.projection));
+        vec4.transformMat4(p, p, this.view);
+        vec4.transformMat4(p, p, mat4.invert(mat4.create(), this.model));
+        console.log(`clicked: screen->[${x},${y}], p-> ${p.join(",")}`);
+    }
+    updateWindowInfo(width: number, height: number) {
+        this.windowInfo[0] = width
+        this.windowInfo[1] = height;
+    }
+    update(elapsed: number, delta: number) {
+        const cameraPos = this.cameraPosition;
+        const velocity = this.velocity;
+        cameraPos[0] += velocity[0] * delta * this.speed;
+        cameraPos[1] += velocity[1] * delta * this.speed;
+        mat4.lookAt(this.view, vec3.fromValues(cameraPos[0], cameraPos[1], 1), vec3.fromValues(cameraPos[0], cameraPos[1], 0), vec3.fromValues(0, 1, 0));
+        const [width, height] = this.windowInfo;
+        mat4.ortho(this.projection, 0, width, height, 0, 1, -1);
+        mat4.fromScaling(this.model, vec3.fromValues(100, 100, 1))
+    }
+    updateDrawobject(drawobject: Drawobject) {
+        const invert = mat4.create();
+        mat4.invert(invert, this.view);
+        drawobject.updateProjection(this.projection);
+        drawobject.updateModel(this.model);
+        drawobject.updateView(invert);
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import Device from './device/Device';
-import { mat3, mat4, vec2, vec3, vec4 } from 'gl-matrix';
+import { vec4 } from 'gl-matrix';
 import Input from './input/Input';
 import Character from './drawobject/Character';
 import Tilemap from './drawobject/Tilemap';
 import Drawobject from './drawobject/Drawobject';
+import Camera from './camera/Camera.js';
 async function start(device: Device) {
 	device.onmessage = (data) => console.log("message from worker", data);
 	device.createWorker("dist/worker/index.js");
 	device.sendmessage && device.sendmessage({ type: "hello" });
 	const context = device.getContext();
 	const input = new Input(device);
+	const camera = new Camera();
 	const drawobjects: Drawobject[] = [];
 	drawobjects.push(new Tilemap(context));
 	drawobjects.push(new Character(context));
@@ -17,54 +19,35 @@ async function start(device: Device) {
 		await drawobject.load(device);
 		drawobject.init();
 	}
-	const projection = mat4.create();
-	const view = mat4.create();
-	const windowInfo = device.getWindowInfo();
-	mat4.ortho(projection, 0, windowInfo[0], windowInfo[1], 0, 1, -1);
-	const cameraPos = vec2.create();
-	const velocity = vec2.create();
-	const model = mat4.create();
-	mat4.scale(model, model, vec3.fromValues(100, 100, 1))
 	input.ondrag = (x, y) => {
-		velocity[0] = x;
-		velocity[1] = -y;
+		camera.setVelocity(x, y);
 	};
 	input.onclick = (x, y) => {
-		const p = vec4.fromValues(x / windowInfo[0], y / windowInfo[1], 0, 1);
-		vec4.sub(p, vec4.mul(p, p, vec4.fromValues(2, 2, 0, 1)), vec4.fromValues(1, 1, 0, 0));
-		p[1] = -p[1];
-		vec4.transformMat4(p, p, mat4.invert(mat4.create(), projection));
-		vec4.transformMat4(p, p, view);
-		vec4.transformMat4(p, p, mat4.invert(mat4.create(), model));
-		console.log(`clicked: screen->[${x},${y}], p-> ${p.join(",")}`);
+		const p = vec4.create()
+		camera.screenToWorld(x, y, p);
 		for (const drawobject of drawobjects) {
 			drawobject.onclick(p[0], p[1]);
 		}
 	}
 	input.onrelease = () => {
-		velocity[0] = 0;
-		velocity[1] = 0;
+		camera.setVelocity(0, 0);
 	}
 	let last = 0;
 	function tick() {
 		input.update();
-		cameraPos[0] += velocity[0];
-		cameraPos[1] += velocity[1];
-		mat4.lookAt(view, vec3.fromValues(cameraPos[0], cameraPos[1], 1), vec3.fromValues(cameraPos[0], cameraPos[1], 0), vec3.fromValues(0, 1, 0));
-		const invert = mat4.create();
-		mat4.invert(invert, view);
 		const now = device.now();
 		const delta = now - last;
 		last = now;
-		context.viewport(0, 0, ...device.getWindowInfo());
-		context.scissor(0, 0, ...device.getWindowInfo());
+		const windowInfo = device.getWindowInfo();
+		camera.updateWindowInfo(...windowInfo)
+		camera.update(now, delta);
+		context.viewport(0, 0, ...windowInfo);
+		context.scissor(0, 0, ...windowInfo);
 		context.clearColor(0.3, 0.3, 0.3, 1);
 		context.clear(context.COLOR_BUFFER_BIT | context.STENCIL_BUFFER_BIT);
 		for (const drawobject of drawobjects) {
-			drawobject.updateProjection(projection);
-			drawobject.updateModel(model);
-			drawobject.updateView(invert);
-			drawobject.update(device.now(), delta);
+			camera.updateDrawobject(drawobject);
+			drawobject.update(now, delta);
 			drawobject.draw();
 		}
 		requestAnimationFrame(tick);
